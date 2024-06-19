a2a17a39b2e52de432984a863c587e4365fe585d
abdelrahman mostafa <abdelrahman.sheta16@gmail.com>
2024-06-19 17:54:18 +0300

    Fix Styling

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import first from '../../public/assets/1.jpg';
 import second from '../../public/assets/2.jpg';
-import { useState } from 'react';
 
 const MainSection = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -18,24 +18,27 @@ const MainSection = () => {
             window.location.href = href; // Proceed with navigation after a delay
         }, 2000); // Simulate a loading time of 2 seconds
     };
+
     return (
-        <section className="flex justify-around">
+        <section className=" bg-white relative w-full h-64 md:h-screen md:flex-3 flex items-center justify-center">
             {isLoading ?
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <div className="loaderMain ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
-                </div>
+                <>
+                    <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+                        <div className="loaderMain ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+                    </div>
+                </>
                 :
                 <>
-                    <div onClick={(e) => handleImageClick(e, "/TextToGaming")}>
+                    <div className="" onClick={(e) => handleImageClick(e, "/TextToGaming")}>
                         <Image src={first} alt="Text to gaming creator" />
                     </div>
-                    <div onClick={(e) => handleImageClick(e, "/TextToNFT")}>
+                    <div className="" onClick={(e) => handleImageClick(e, "/TextToNFT")}>
                         <Image src={second} alt="NFT/2D to gaming asset" />
                     </div>
                 </>
             }
         </section>
-    )
+    );
 }
 
-export default MainSection
+export default MainSection;
